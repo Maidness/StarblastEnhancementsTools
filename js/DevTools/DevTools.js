71e05c489b85ac4d74396a2d255c4be607bd3fa8
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-02-28 08:33:08 +0700

    Added DevTools Panel (beta)

---
+++ js/DevTools/DevTools.js
@@ -0,0 +1,4 @@
+chrome.devtools.panels.create("SET Portable",
+                              null,
+                              "/html/DevTools/DevTools.html",
+                              function(panel) { console.log("hello from callback"); });
