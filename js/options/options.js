554fc9ee6a4365ef6dca5d0e159da7e87ba3b02d
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-22 17:58:37 +0700

    eh... code errors

--- js/options/options.js
+++ js/options/options.js
@@ -17,7 +17,7 @@ document.querySelector("#main").innerText=text("settings");
 let title=document.createElement("title");
 title.innerText=text("settings")+" - Starblast Enhancements Tools";
 document.head.appendChild(title);
-document.querySelector("#changelog_set").innerHTML=text("changelog_set",'<a href="#" id="full-log title="'+text("changelog_set_desc")+'">\"'+text("fulllog")+'\"</a>:&nbsp');
+document.querySelector("#changelog_set").innerHTML=text("changelog_set",'<a href="#" id="full-log" title="'+text("changelog_set_desc")+'">\"'+text("fulllog")+'\"</a>:&nbsp');
 document.querySelector("#onlog").options[0].innerText=text("changelog_set_opt_1");
 document.querySelector("#onlog").options[1].innerText=text("changelog_set_opt_2","changelog.txt");
 document.querySelector("#full-log").addEventListener('click',function(activeTab)
