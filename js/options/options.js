5697dfd72a5141b98aabc6e889744b9d20129bbf
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-22 13:10:29 +0700

    done locales for en,es,ms,pt,tl and vi

--- js/options/options.js
+++ js/options/options.js
@@ -1,11 +1,9 @@
-window.browser = (function () {
-  return window.msBrowser ||
-    window.browser ||
-    window.chrome;
-})();
-function text(a,b)
+function text(a,b,...c)
 {
-	return browser.i18n.getMessage(a)+(b||"");
+	let i = 0;
+	return chrome.i18n.getMessage(a).replace(/%s/g,function(v){
+		return c[(i<c.length)?(i++):i]||"";
+	})+(b||"");
 }
 function saveOptions(e) {
   e.preventDefault();
@@ -19,9 +17,9 @@ document.querySelector("#main").innerText=text("settings");
 let title=document.createElement("title");
 title.innerText=text("settings")+" - Starblast Enhancements Tools";
 document.head.appendChild(title);
-document.querySelector("#changelog_set").innerHTML=text("changelog_set").replace(/<[^>]+>/,'<a href="#" id="full-log title="'+text("changelog_set_desc")+'">\"'+text("fulllog")+'\"</a>:&nbsp');
+document.querySelector("#changelog_set").innerHTML=text("changelog_set",'<a href="#" id="full-log title="'+text("changelog_set_desc")+'">\"'+text("fulllog")+'\"</a>:&nbsp');
 document.querySelector("#onlog").options[0].innerText=text("changelog_set_opt_1");
-document.querySelector("#onlog").options[1].innerText=text("changelog_set_opt_2").replace(/<[^>]+>/,"Changelog.txt");
+document.querySelector("#onlog").options[1].innerText=text("changelog_set_opt_2","changelog.txt");
 document.querySelector("#full-log").addEventListener('click',function(activeTab)
 {
   if (localStorage.check==1) window.open('https://starblast.io/changelog.txt', '_blank');
