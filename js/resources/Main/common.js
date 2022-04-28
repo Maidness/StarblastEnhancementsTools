bfd9bba77317af8b3c8b9c972772c73a83e865d2
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-04-29 02:31:31 +0900

    small fix

--- js/resources/Main/common.js
+++ js/resources/Main/common.js
@@ -6,6 +6,6 @@ let g = setInterval(function () {
   catch (e) {}
 }, 1);
 
-let x = Object.values(Object.values(module.exports.settings).find(v => v && v.mode)).find(v => v && v.startModdingMode);
-if (x) x.startGame = eval("(" + x.startGame.toString().replace(/\.toUpperCase\(\)/g, "") + ")");
+let x = Object.values(Object.values(module.exports.settings).find(v => v && v.mode)).find(v => v && "function" == typeof v.startModdingMode);
+if (x) x.startGame = Function("return " + x.startGame.toString().replace(/\.toUpperCase\(\)/g, ""))();
 ChatPanel.prototype.typed = Function("return " + ChatPanel.prototype.typed.toString().replace(/>=\s*4/, " >= 5"))()
